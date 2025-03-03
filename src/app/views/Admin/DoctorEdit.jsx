f9789bda47c05a999136d393df33d13cec78c337
shyam <shyamrohan115@gmail.com>
2025-03-03 10:23:55 +0530

    file

--- src/app/views/Admin/DoctorEdit.jsx
+++ src/app/views/Admin/DoctorEdit.jsx
@@ -124,50 +124,50 @@ const DoctorEdit = () => {
 
 
   //======================================================================================================================//
-  const CHARACTER_LIMIT = 100;
+  // const CHARACTER_LIMIT = 100;
 
-  const [numberEmptyError, setNumberEmptyError] = useState(false);
-  const [messageEmptyError, setMessageEmptyError] = useState(false);
+  // const [numberEmptyError, setNumberEmptyError] = useState(false);
+  // const [messageEmptyError, setMessageEmptyError] = useState(false);
 
-  const [formData, setFormData] = useState({
-    mobileNumber: "",
-    message: "",
-  });
+  // const [formData, setFormData] = useState({
+  //   mobileNumber: "",
+  //   message: "",
+  // });
 
-  const { mobileNumber, message } = formData;
+  // const { mobileNumber, message } = formData;
 
-  const onChange = (e) => {
-    e.preventDefault();
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  // const onChange = (e) => {
+  //   e.preventDefault();
+  //   setFormData({
+  //     ...formData,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (mobileNumber.length < 1) {
-      setNumberEmptyError(true);
-      setTimeout(() => setNumberEmptyError(false), 3000);
-    } else if (message.length < 1) {
-      setMessageEmptyError(true);
-      setTimeout(() => setMessageEmptyError(false), 3000);
-    } else {
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (mobileNumber.length < 1) {
+  //     setNumberEmptyError(true);
+  //     setTimeout(() => setNumberEmptyError(false), 3000);
+  //   } else if (message.length < 1) {
+  //     setMessageEmptyError(true);
+  //     setTimeout(() => setMessageEmptyError(false), 3000);
+  //   } else {
 
-      // Regex expression to remove all characters which are NOT alphanumeric 
-      let number = mobileNumber.replace(/[^\w\s]/gi, "").replace(/ /g, "");
+  //     // Regex expression to remove all characters which are NOT alphanumeric 
+  //     let number = mobileNumber.replace(/[^\w\s]/gi, "").replace(/ /g, "");
 
-      // Appending the phone number to the URL
-      let url = `https://web.whatsapp.com/send?phone=${number}`;
+  //     // Appending the phone number to the URL
+  //     let url = `https://web.whatsapp.com/send?phone=${number}`;
 
-      // Appending the message to the URL by encoding it
-      url += `&text=${encodeURI(message)}&app_absent=0`;
+  //     // Appending the message to the URL by encoding it
+  //     url += `&text=${encodeURI(message)}&app_absent=0`;
 
-      // Open our newly created URL in a new tab to send the message
-      window.open(url);
+  //     // Open our newly created URL in a new tab to send the message
+  //     window.open(url);
 
-    }
-  };
+  //   }
+  // };
   return (
 
 
@@ -447,70 +447,6 @@ const DoctorEdit = () => {
       </Formik>
 
 
-      <div className="communication">
-        <div className="whatsapp-card app">
-          {/* Title Section */}
-          <div className="title flex_middle">
-            <WhatsAppIcon sx={{ marginRight: "0.5em" }} />
-            <div>Send Message</div>
-          </div>
-
-          {/* Error Messages */}
-          {numberEmptyError && <div className="errors">Mobile number cannot be empty!</div>}
-          {messageEmptyError && <div className="errors">Message cannot be empty!</div>}
-          {!numberEmptyError && !messageEmptyError && <div className="errors-null">.</div>}
-
-          {/* Mobile Number Input */}
-          <div className="search_contact app">
-            <TextField
-              error={numberEmptyError}
-              label="Mobile Number"
-              placeholder="Mobile Number"
-              name="mobileNumber"
-              value={mobileNumber}
-              onChange={onChange}
-              size="small"
-              sx={{ margin: "1em 0em", width: "100%" }}
-              required
-            />
-          </div>
-
-          {/* Message Input */}
-          <div className="message app" style={{ marginTop: "1.5em" }}>
-            <TextField
-              multiline
-              maxRows={4}
-              label="Message"
-              placeholder="Hi! Sending a message from React...."
-              size="small"
-              name="message"
-              value={message}
-              onChange={onChange}
-              required
-              error={message.length > CHARACTER_LIMIT || messageEmptyError}
-              helperText={
-                message.length > CHARACTER_LIMIT
-                  ? "Max length exceeded"
-                  : `${message.length}/${CHARACTER_LIMIT}`
-              }
-              sx={{ width: "100%" }}
-            />
-          </div>
-
-          {/* Send Button */}
-          <div style={{ marginTop: "1.5em" }}>
-            <Button
-              onClick={onSubmit}
-              variant="outlined"
-              color="success"
-              size="small"
-              disabled={numberEmptyError || messageEmptyError}
-            >
-              Send
-            </Button>
-          </div>
-        </div>
-      </div>
 </>
   ) : (
       false
